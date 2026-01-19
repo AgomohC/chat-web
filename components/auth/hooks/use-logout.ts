eec0c249d3825ab2aef64e2382db9004c8015082
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2026-01-19 20:14:26 +0100

    feat(auth): add useLogout hook for handling user logout

---
+++ components/auth/hooks/use-logout.ts
@@ -0,0 +1,26 @@
+import { useAuthControllerLogout } from "@/core/api-client/neurachatComponents";
+import { COOKIE_STORAGE_KEYS } from "@/lib/constants";
+import { useQueryClient } from "@tanstack/react-query";
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const { mutateAsync, status } = useAuthControllerLogout();
+
+  const logout = async () => {
+    mutateAsync({}).then(() => {
+      deleteCookie(COOKIE_STORAGE_KEYS.ACCESS_TOKEN);
+    });
+    router.refresh();
+    setTimeout(() => {
+      queryClient.clear();
+    }, 50);
+  };
+
+  return {
+    logout,
+    loading: status === "pending",
+  };
+};
