47cef190651654900645a61b9c7b383bbbcb28d6
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2026-01-20 04:37:01 +0100

    feat: add prettier config and new dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    optimizePackageImports: ["lucide-react"],
-  },
-};
+    optimizePackageImports: ["lucide-react", "@untitledui/icons"]
+  }
+}
 
-export default nextConfig;
+export default nextConfig
