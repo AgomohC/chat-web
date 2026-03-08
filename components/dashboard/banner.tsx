e01da55fdc55995687af2ec0e39096a522a8f618
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2026-03-08 13:17:50 +0100

    fix(dashboard): replace AVIF banner with JPG for better compatibility

--- components/dashboard/banner.tsx
+++ components/dashboard/banner.tsx
@@ -1,9 +1,18 @@
+import BannerImage from "@/public/banner.jpg"
+import Image from "next/image"
+
 export const Banner = () => {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 space-y-2">
-      <div className="bg-[url('/banner.avif')] bg-cover bg-center bg-no-repeat h-[min(40dvh,40dvw)] w-[max(40dvw,40dvh)] " />
-
-      <h1 className="text-xl text-center uppercase font-semibold">
+      <Image
+        src={BannerImage}
+        alt="banner"
+        className="h-auto w-[max(40dvw,40dvh)]"
+        loading="eager"
+        width={3200}
+        height={2400}
+      />
+      <h1 className="lg:text-xl text-base text-center uppercase font-semibold">
         Select a conversation to continue
       </h1>
     </div>
